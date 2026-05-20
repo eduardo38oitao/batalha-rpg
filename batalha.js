c7a9a70ca948258fd16b6d49ad43daa40431c3d7
eduardo38oitao <klen.soares@escola.pr.gov.br>
2026-05-20 11:52:06 -0300

    Add files via upload

--- batalha.js
+++ batalha.js
@@ -9,5 +9,27 @@ if(!arma){
 
     const dado = Math.floor(Math.ramdom() * 20)+ 1;
     let resultado = `🎲 Dado: ${dado} | <strong>${classe}</strong> com <strong> ${arma}</strong>`;
+log.innerHTML = resultado
+
+
+switch(classe){
+    case"guerreiro":
+    if(arma.toLowerCase() === "espada" && dado>5){
+        resultado += "<span class='sucesso'> SUCESSO: Voce decapitou o monstro com um golpe de espada </span>"
+    }else if(dado<15){
+        resultado += "<span class= 'sucesso'> VITÓRIA: Na froça bruta, Voce esmagou o seu inimigo </span>"
+    }else{
+        resultado += "<span classe = 'derrota'> DERROTA:O monstro ceifou sua vida: Voce morreu por hemorragia</span>"}
+        break;
+        case "mago":
+            if(arma.toLowerCase() === "cajado" && dado> 8){
+                resultado += "<span class= 'sucesso' SUCESSO: Voce incenerou  o monstro até a morte></span>"
+            }else if(dado = 19){
+                resultado +="<span class= 'sucesso' VITORIA: Voce acertou um ataque crítico e molestou o monstro></span>"
+            }
 }
+
  
+}
+
+
