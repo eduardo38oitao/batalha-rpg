e080e035010eb6b445d54c7f12e992a4eeb5a4f8
eduardo38oitao <klen.soares@escola.pr.gov.br>
2026-05-21 09:11:34 -0300

    Add files via upload

--- batalha.js
+++ batalha.js
@@ -15,18 +15,26 @@ log.innerHTML = resultado
 switch(classe){
     case"guerreiro":
     if(arma.toLowerCase() === "espada" && dado>5){
-        resultado += "<span class='sucesso'> SUCESSO: Voce decapitou o monstro com um golpe de espada </span>"
+        resultado += "<span class='sucesso'> SUCESSO: Voce decapitou o monstro com um golpe de espada "+arma+"</span>"
     }else if(dado<15){
-        resultado += "<span class= 'sucesso'> VITÓRIA: Na froça bruta, Voce esmagou o seu inimigo </span>"
+        resultado += "<span class= 'sucesso'> VITÓRIA: Na froça bruta, Voce esmagou o seu inimigo "+arma+" </span>"
     }else{
         resultado += "<span classe = 'derrota'> DERROTA:O monstro ceifou sua vida: Voce morreu por hemorragia</span>"}
         break;
-        case "mago":
+    case "mago":
             if(arma.toLowerCase() === "cajado" && dado> 8){
-                resultado += "<span class= 'sucesso' SUCESSO: Voce incenerou  o monstro até a morte></span>"
-            }else if(dado = 19){
-                resultado +="<span class= 'sucesso' VITORIA: Voce acertou um ataque crítico e molestou o monstro></span>"
-            }
+                resultado += "<span class= 'critico' VITORIA: Voce acertou um ataque crítico com o cajado e explodiu o monstro ></span>"
+            }else if(dado <=18){
+                resultado += "<span class= 'sucesso' SUCESSO: Voce usou"+arma+"para matar o monstro></span>"
+            }else{
+                resultado +="<span class 'derrota' DERROTA: Voce demorou demais para usar "+arma+" e morreu></span>" }
+                break;
+    case "arqueiro":
+        if(arma.toLowerCase()=== "arco"&& dado>10){
+            resultado +="<span class='sucesso' VITORIA: Voce matou o monstro com com apenas uma flexa ></span>"
+        }else if(dado<=17){
+            resultado +=
+        }
 }
 
  
