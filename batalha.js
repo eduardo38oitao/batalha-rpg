87cef4623e2174c1068db17a378333c321d88350
eduardo38oitao <klen.soares@escola.pr.gov.br>
2026-05-25 08:37:10 -0300

    Add files via upload

--- batalha.js
+++ batalha.js
@@ -1,48 +1,53 @@
-function lutar(){
-const classe = document.getElementById('classe').value;
-const arma = document.getElementById ('arma').value.trim();
-const log = document.getElementById('log');
+function lutar() {
 
-if(!arma){
-    log.innerHTML= '❌Por favor, selecione uma arma para lutar!';
-    return;}
-
-    const dado = Math.floor(Math.ramdom() * 20)+ 1;
-    let resultado = `🎲 Dado: ${dado} | <strong>${classe}</strong> com <strong> ${arma}</strong>`;
-log.innerHTML = resultado
-
-
-switch(classe){
-    case"Guerreiro":
-    if(arma.toLowerCase() === "Espada" && dado>5){
-        resultado += "<span class='sucesso'> SUCESSO: Voce decapitou o monstro com um golpe de espada </span>";
-    }else if(dado>=15){
-        resultado += "<span class= 'sucesso'> VITÓRIA: Na froça bruta, Voce esmagou o seu inimigo "+arma+" </span>";
-    }else{
-        resultado += "<span classe = 'derrota'> DERROTA:O monstro ceifou sua vida: Voce morreu por hemorragia</span>";}
-        break;
+    const classe = document.getElementById('classe').value;
+    const arma = document.getElementById('arma').value.trim();
+    const log = document.getElementById('log');
+    
+    if (!arma) {
+    log.innerHTML = "Você esqueceu de equipar uma arma!";
+    return;
+    }
+    
+    
+    const dado = Math.floor(Math.random() * 20) + 1;
+    let resultado = `🎲 Dado: ${dado} | <strong>${classe}</strong> com <strong>${arma}</strong><br><br>`;
+    
+    switch (classe) {
+    case "Guerreiro":
+    if (arma.toLowerCase() === "espada" && dado > 5) {
+    resultado += "<span class='sucesso'>SUCESSO: Você decapitou o monstro com um golpe de espada!</span>";
+    } else if (dado > 15) {
+    resultado += "<span class='sucesso'>VITÓRIA: Na força bruta, você esmagou o inimigo com seu " + arma + "!</span>";
+    } else {
+    resultado += "<span class='derrota'>DERROTA: O monstro foi mais rápido. Seu " + arma + " não foi rapida o sufiente!</span>";
+    }
+    break;
+    
     case "Mago":
-            if(arma.toLowerCase() === "Cajado" && dado> 8){
-                resultado += "<span class= 'critico' VITORIA: Voce acertou um ataque crítico com o cajado e explodiu o monstro ></span>";
-            }else if(dado >=18){
-                resultado += "<span class= 'critico' SUCESSO: Voce usou"+arma+"para matar o monstro></span>";
-            }else{
-                resultado +="<span class 'derrota' DERROTA: Voce demorou demais para usar "+arma+" e morreu></span>"; }
-                break;
+    if (arma.toLowerCase() === "cajado" && dado > 8) {
+    resultado += "<span class='critico'>CRÍTICO: Seu cajado brilhou e desintegrou o alvo!</span>";
+    }
+    else if (dado === 20) {
+    resultado += "<span class='critico'>MILAGRE: Você deu uma cajadada física e nocauteou o monstro!</span>";}
+    else {
+    resultado += "<span class='derrota'>FALHA: Você errou as palavras mágicas e o feitiço explodiu na sua mão!</span>";}
+    break;
+    
     case "Arqueiro":
-        if(arma.toLowerCase()=== "Arco"&& dado>10){
-            resultado +="<span class='sucesso' VITORIA: Voce matou o monstro com com apenas uma flexa ></span>";
-        }else if(dado>=17){
-            resultado +="<span class='win' VITORIA: Voce matou o monstro humilhantemente com "+arma+"></span>";
-        }else{
-            resultado +="<span class= 'derrota' DERROTA: O inimigo desviu do seus ataque e o matou></span>"; }
-            break;
-
-            default:
-            resultado += "Voce ficou paralisado de medo";
-        }
-        log.innerHTML = resultado;
- 
+    if (arma.toLowerCase() === "arco" && dado > 10) {
+    resultado += "<span class='sucesso'>PRECISÃO: Uma flecha certeira no olho do monstro!</span>";
+    } 
+    else if (dado > 18) {
+    resultado += "<span class='sucesso'>SORTE: Você disparou sua " + arma + " e acertou sem nem olhar!</span>";}
+    else {
+    resultado += "<span class='derrota'>ERROU: O monstro desviou e agora está correndo atrás de você!</span>";}
+    break;
+    
+    default:
+    resultado += "Você ficou paralisado de medo.";
 }
-
-
+    
+    
+    log.innerHTML = resultado;
+    }
